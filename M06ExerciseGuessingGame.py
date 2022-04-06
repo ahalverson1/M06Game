1fc7be810b61842bf5b884751ffbb6995a341da4
Alysia Halverson <ahalverson@dev-10.com>
2022-04-06 15:34:42 -0500

    completed part 2

--- M06ExerciseGuessingGame.py
+++ M06ExerciseGuessingGame.py
@@ -5,7 +5,7 @@ letters = len(word)
 print(f'There are ' + str(letters) + ' letters in this word')
 
 num_guesses = 7
-guesses = []
+lettersGuessed = ''
 win_game = False
 
 while win_game == False and num_guesses > 0:
@@ -14,24 +14,35 @@ while win_game == False and num_guesses > 0:
     guess_word_result = ''
     guesses = ''
     if type(user_input) is int:
-        guess_word_result = 'Please input a letter or word guess.'
+        guess_word_result = 'Please input a letter or word guess. You have ' + str(num_guesses) + ' guesses left.'
     if guess == word:
         guess_word_result = 'Correct word! You win!'
         win_game = True
-    elif user_input is not int and guess in guesses:
-        guess_word_result = 'You have already guessed this letter. Pick again.'
+    elif user_input is not int and guess in lettersGuessed:
+        guess_word_result = 'You have already guessed this letter. Pick again. You have ' + str(num_guesses) + ' guesses left.'
     elif user_input is not int and guess in word:
-        guess_word_result = 'Good guess!'
-        print(num_guesses)
+        guess_word_result = 'Good guess! You have ' + str(num_guesses) + ' guesses left.'
     elif user_input is not int and guess not in word:
-        num_guesses -= 1
-        guess_word_result = 'Wrong guess.'
-        print(f'You have ' + str(num_guesses) + ' guesses left.')
+        num_guesses = num_guesses - 1
+        guess_word_result = 'Wrong guess. You have ' + str(num_guesses) + ' guesses left.'
     else:
         guess_word_result = ''
 
     print(guess_word_result)
 
+    lettersGuessed = lettersGuessed + guess
+    wrongLetterCount = 0
+
+    for letter in word:
+        if letter in lettersGuessed:
+            print(f'{letter}', end="")
+        else:
+            print("_", end="")
+            wrongLetterCount += 1
+    print("")
+
+    if wrongLetterCount == 0:
+        print(f"Congrats! The word was {word}.")
 if win_game == False and num_guesses == 0:
     print('Game over.')
         
